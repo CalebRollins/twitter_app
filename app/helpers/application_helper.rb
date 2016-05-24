c0c5b4f65259673170ce1e17872b3d2390283a5f
Caleb Rollins <crollins321@yahoo.com>
2016-05-24 04:30:04 +0000

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  # ApplicationHelper provides methods so you don't have to include them
+  # Returns the full title on a per-page basis.
+  def full_title(page_title = '')
+    base_title = "Twitter Happier"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
