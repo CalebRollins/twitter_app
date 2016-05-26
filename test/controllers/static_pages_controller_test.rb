13e1879ee2bd96c0b15df0376bf82046e2f45383
Caleb Rollins <crollins321@yahoo.com>
2016-05-26 06:06:42 +0000

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -18,4 +18,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "About | Twitter Happier"
   end
+  
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Twitter Happier"
+  end
 end
